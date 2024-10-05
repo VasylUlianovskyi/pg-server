81ade727c6c91edc442a7db426e11e31465d92c6
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-10-05 19:18:43 +0300

    add paginate middleware

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const { usersController } = require('./controllers/index,');
-const { errorHandlers, validate } = require('./middleware');
+const { errorHandlers, validate, paginate } = require('./middleware');
+const queryParser = require('query-parser-express');
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  queryParser({
+    parseBoolean: true,
+    parseNumber: true,
+  })
+);
+
 app.post('/users', validate.validationOnCreate, usersController.createUser);
-app.get('/users', usersController.getAllUsers);
+app.get('/users', paginate.paginateUsers, usersController.getAllUsers);
 app.get('/users/:userId', usersController.getUserById);
 app.patch('/users/:userId', usersController.updateUserById);
 app.delete('/users/:userId', usersController.deleteUserById);
