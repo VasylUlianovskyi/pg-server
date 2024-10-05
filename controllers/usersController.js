a99bb222b0a5353ea3248103d1723665dc22ebda
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-10-05 17:31:48 +0300

    Add CRUD

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const createHttpError = require('http-errors');
 const { User } = require('./../models');
 
 module.exports.createUser = async (req, res, next) => {
@@ -11,11 +12,64 @@ module.exports.createUser = async (req, res, next) => {
 };
 
 module.exports.getAllUsers = async (req, res, next) => {
-  res.status(501).send('Not Implemented');
+  const { page, results } = req.query;
+
+  const limit = results;
+  const offset = (page - 1) * results;
+
+  try {
+    const foundUsers = await User.getAll(limit, offset);
+
+    res.status(200).send(foundUsers);
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports.getUserById = async (req, res, next) => {};
+module.exports.getUserById = async (req, res, next) => {
+  const { userId } = req.params;
 
-module.exports.updateUserById = async (req, res, next) => {};
+  try {
+    const foundUser = await User.getById(userId);
+    if (!foundUser) {
+      // res.status(404).send('User Not Found');
+      return next(createHttpError(404, 'User Not Found'));
+    }
+    res.status(200).send(foundUser);
+  } catch (err) {
+    next(err);
+  }
+};
 
-module.exports.deleteUserById = async (req, res, next) => {};
+module.exports.updateUserById = async (req, res, next) => {
+  const {
+    params: { userId },
+    body,
+  } = req;
+
+  try {
+    const updatedUser = await User.updateById(body, userId);
+
+    if (!updatedUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+
+    res.status(200).send(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteUserById = async (req, res, next) => {
+  const { userId } = req.params;
+
+  try {
+    const foundUser = await User.deleteById(userId);
+    if (!foundUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+    res.status(204).send('User Deleted');
+  } catch (err) {
+    next(err);
+  }
+};
